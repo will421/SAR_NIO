6ae7043e066cd7f4720381624905e3ea13dc4445
will421 <willou.421@gmail.com>
2014-09-23 10:55:49 +0200

    Premier send

---
+++ nio.engine/src/nio/test/PingPongServer.java
@@ -0,0 +1,58 @@
+package nio.test;
+
+import java.io.IOException;
+
+import nio.engine.AcceptCallback;
+import nio.engine.CNioEngine;
+import nio.engine.NioChannel;
+import nio.engine.NioEngine;
+import nio.engine.NioServer;
+
+public class PingPongServer implements AcceptCallback,Runnable
+{
+	static final String prefServer = "[Server]";
+	int port;
+
+
+	public PingPongServer(int p) {
+		port = p;
+	}
+
+	@Override
+	public void closed(NioChannel channel) {
+		System.out.println(prefServer+"AcceptCallback closed");
+
+	}
+
+	@Override
+	public void accepted(NioServer server, NioChannel channel) {
+		System.out.println(prefServer+"AcceptCallback accepted");
+
+	}
+
+	@Override
+	public void run() {
+		System.out.println(prefServer+"Server launched with port= "+port);
+		NioEngine engine = null;
+		try {
+			engine = new CNioEngine();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		try {
+			engine.listen(port,this);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		engine.mainloop();
+
+	}	
+
+
+}
